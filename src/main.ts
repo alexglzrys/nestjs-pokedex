1c8ba5260561ce9a1b4d42ea5993c99917a3f66a
Alejandro González Reyes <alexglzrys@gmail.com>
2022-12-04 13:19:10 -0600

    POST - Recibir y validar la data (dto)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar a nivel Global la Validación de Pipes (Validar DTOs)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   // Colocar un prefijo global a todos los endpoints de mi API REST
   app.setGlobalPrefix('api/v2');
--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+// npm i class-validator class-transformer
+
+// DTO para validar la data (body request) al momento de registrar un Pokemon
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
