df9b2a64c27aa3527f38db7a696c9dc294930b31
Alejandro González Reyes <alexglzrys@gmail.com>
2022-12-04 11:24:23 -0600

    servir contenido estático

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    // Importar módulo para servir archivos estáticos.
+    // Es similar a cuando se trabaja con ExpressJS, se tiene que especificar dónde están alojados los assets del proyecto.
+    // Para este caso solo nos interesa especificar dónde se encuentra la carpeta con la aplicación SPA del proyecto
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
 })
 export class AppModule {}
